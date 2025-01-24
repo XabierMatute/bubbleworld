9d0995158ef808c40f98f558173010cbe150c657
xmatute- <xmatute-@student.42urduliz.com>
2025-01-24 22:08:36 +0100

    burbujeo wip

--- protogamejam/player.gd
+++ protogamejam/player.gd
@@ -2,12 +2,31 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
-@export var bubble_speed = 1.0 / 4
 
-var air = 1
+signal popped
+
+@export var initial_air = 100
+@export var air_decrement = 1
+@export var base_jump_air_cosumption = 20
+@export var jump_air_cosumption_increment = 5
+var jump_air_cosumption = base_jump_air_cosumption
+
+var air
+
+var alive = true
+
+func _ready() -> void:
+	alive = true
+	air = initial_air
+
+func get_bubble_percentage():
+	return float(air) / initial_air
+
 
 func _process(delta: float) -> void:
-	if velocity.x != 0:
+	if velocity.y < 0:
+		$AnimatedSprite2D.play("swim")
+	elif velocity.x != 0:
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
@@ -15,20 +34,40 @@ func _process(delta: float) -> void:
 	else:
 		$AnimatedSprite2D.stop()
 	
-	$Bubble.scale -= Vector2(delta * bubble_speed, delta * bubble_speed)
-	if $Bubble.scale.x < 0 or $Bubble.scale.y < 0:
-		$Bubble.scale = Vector2(0,0)
+
+	
+	air -= air_decrement  * delta
+	if air < 0:
+		air = 0
 		emit_signal("popped")
+		# debug
+		air = 111
+	$Bubble.scale = Vector2(1 ,1) * (get_bubble_percentage())
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * (1 - get_bubble_percentage()) * 2
+		
+	#if Input.is_action_just_pressed("ui_up"):
+		#velocity -= velocity / 2
+	#if Input.is_action_just_pressed("ui_down"):
+		#velocity += velocity / 2
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_air_cosumption = base_jump_air_cosumption
+		air -= jump_air_cosumption
+	
+	# Handle double jump??	
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_air_cosumption += jump_air_cosumption_increment
+		air -= jump_air_cosumption
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -37,5 +76,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	direction = Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity.y += direction * SPEED/100
+	#else:
+		#velocity.y += move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
