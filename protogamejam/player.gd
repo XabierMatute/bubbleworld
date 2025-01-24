98b0a7a8756f8bdd032f8847dd053508fd91618e
xmatute- <xmatute-@student.42urduliz.com>
2025-01-24 22:30:13 +0100

    burbujea

--- protogamejam/player.gd
+++ protogamejam/player.gd
@@ -76,9 +76,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	direction = Input.get_axis("ui_up", "ui_down")
-	if direction:
-		velocity.y += direction * SPEED/100
+	#direction = Input.get_axis("ui_up", "ui_down")
+	#if direction:
+		#velocity.y += direction * SPEED/100
 	#else:
 		#velocity.y += move_toward(velocity.y, 0, SPEED)
 
