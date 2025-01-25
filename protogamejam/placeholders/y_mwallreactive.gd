94355fc498cc6fe9916b344e09586b13a6d0fd14
xmatute- <xmatute-@student.42urduliz.com>
2025-01-25 12:50:36 +0100

    .

---
+++ protogamejam/placeholders/y_mwallreactive.gd
@@ -0,0 +1,37 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Area2D/AnimatedSprite2D.play("paloma")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+#func _on_area_2d_area_entered(area: Area2D) -> void:
+	#print("_on_area_2d_area_entered", area)
+	#$Area2D/AnimatedSprite2D.play("cepillos")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("_on_area_2d_body_entered", body)
+	
+		
+	$Area2D/AnimatedSprite2D.play("cepillos")
+	if body.dashing:
+		body.air += 50
+		$Area2D/AnimatedSprite2D.play("dinosaurio")
+	else:
+		body.air -= 50
+		
+	
+# Cuando un cuerpo sale, revisa si no quedan más cuerpos y pausa la animación.
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("_on_area_2d_body_exited", body)
+
+	# Comprueba si no hay cuerpos en el área
+	if $Area2D.get_overlapping_bodies().size() == 0:
+		$Area2D/AnimatedSprite2D.stop()
--- protogamejam/player.gd
+++ protogamejam/player.gd
@@ -5,73 +5,80 @@ extends CharacterBody2D
 
 signal popped
 
-@export var initial_air = 100
+@export var initial_air := 100.0
 @export var air_decrement = 1
 @export var base_jump_air_cosumption = 20
+@export var dash_air_cosumption = 20
 @export var jump_air_cosumption_increment = 5
 var jump_air_cosumption = base_jump_air_cosumption
+@export var dash_velocity = 1000
 
-var air
+var air := initial_air
 
 var alive = true
+
+var looking_left = false
+
+var dashing = false
 
 func _ready() -> void:
 	alive = true
 	air = initial_air
+	$Bubble.scale *= 2
+	$AnimatedSprite2D.animation = "walk"
 
 func get_bubble_percentage():
 	return float(air) / initial_air
 
-
-func _process(delta: float) -> void:
+func handle_animation():
+	$AnimatedSprite2D.flip_v = false
+	# See the note below about the following boolean assignment.
+	$AnimatedSprite2D.flip_h = looking_left
+	if dashing:
+		$AnimatedSprite2D.play("dash")
+		return
+	if velocity.x < 0:
+		looking_left = true
+	elif velocity.x > 0:
+		looking_left = false
 	if velocity.y < 0:
 		$AnimatedSprite2D.play("swim")
 	elif velocity.x != 0:
 		$AnimatedSprite2D.play("walk")
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
 	else:
 		$AnimatedSprite2D.stop()
 	
 
-	
+
+func _process(delta: float) -> void:
+	handle_animation()
 	air -= air_decrement  * delta
 	if air < 0:
 		air = 0
 		emit_signal("popped")
 		# debug
 		air = 111
-	$Bubble.scale = Vector2(1 ,1) * (get_bubble_percentage())
+	$Bubble.scale = Vector2(2 ,2) * ((get_bubble_percentage()) / 2 + 0.5)
 
 @export var dash = 400
+
+func get_player_gavity():
+	return get_gravity() * (1 - get_bubble_percentage())
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * (1 - get_bubble_percentage()) * 2
-		
-	#if Input.is_action_just_pressed("ui_up"):
-		#velocity -= velocity / 2
-	#if Input.is_action_just_pressed("ui_down"):
-		#velocity += velocity / 2
-		
+		velocity +=  delta * get_player_gavity()
+	
+	# ir para abajo en plan tetris
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y = 0 - JUMP_VELOCITY
 
 	## Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jump_air_cosumption = base_jump_air_cosumption
 		air -= jump_air_cosumption
-	#
-	## Handle double jump??	
-	#if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-		#jump_air_cosumption += jump_air_cosumption_increment
-		#air -= jump_air_cosumption
-		
-
-	
-		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -80,32 +87,25 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	#direction = Input.get_axis("ui_up", "ui_down")
-	#if direction:
-		#velocity.y += direction * SPEED/100
-	#else:
-		#velocity.y += move_toward(velocity.y, 0, SPEED)
-
+		
 		# Handdle dash
-	#if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
-		#var xdirection := Input.get_axis("ui_left", "ui_right") #TODO: mirar controles de joystick
-		#var ydirection := Input.get_axis("ui_left", "ui_right")
-		#print("dash", velocity)
-		#velocity += Vector2(xdirection, ydirection).normalized() * dash
-		#print(velocity)
-	if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
-		var xdirection := Input.get_axis("ui_left", "ui_right")  # Movimiento en eje X
-		var ydirection := Input.get_axis("ui_up", "ui_down")    # Movimiento en eje Y (joystick o teclado)
-
-		if xdirection != 0 or ydirection != 0:  # Solo aplica dash si hay entrada de movimiento
-			var dash_vector = Vector2(xdirection, ydirection).normalized() * dash
-			velocity = dash_vector # Añade el dash a la velocidad actual
-			print("Dash vector:", dash_vector, "New velocity:", velocity)
-			
-			# Activar partículas
-			$DashParticles.global_position = global_position  # Posiciona las partículas en el personaje
-			$DashParticles.emitting = true
-
-			print("Dash vector:", dash_vector, "New velocity:", velocity)
+	if Input.is_action_just_pressed("dash"):
+		dashing = true
+		$DashTimer.start()
+		air -= dash_air_cosumption
+	
+	if dashing:
+		print("dashing")
+		velocity.y = 0
+		if looking_left:
+			velocity.x = -dash_velocity
+		else:
+			velocity.x = dash_velocity
 
 	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	print("ya no dashing")
+	dashing = false
+	$AnimatedSprite2D.animation = "walk"
